87665f8c4e86922d93a77acb20ae90d5909624e0
ThaxK <143111369+ThaxK@users.noreply.github.com>
2023-11-29 21:34:55 -0500

    Add files via upload
    Hilary cambios

---
+++ FromRegistrarCliente.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PROYECTO_MINIMARKET
+{
+    partial class FromRegistrarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.NoDocumentoCliente = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TelefonoCliente = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ApellidoCliente = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NombreCliente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DireccionCliente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRegsitrarCliente = new System.Windows.Forms.Button();
+            this.CBoxTipoDocuCliente = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CorreoCliente = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CorreoCliente);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.CBoxTipoDocuCliente);
+            this.groupBox1.Controls.Add(this.btnRegsitrarCliente);
+            this.groupBox1.Controls.Add(this.NoDocumentoCliente);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.TelefonoCliente);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.ApellidoCliente);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.NombreCliente);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.DireccionCliente);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 426);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "registrarCliente";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // NoDocumentoCliente
+            // 
+            this.NoDocumentoCliente.Location = new System.Drawing.Point(140, 81);
+            this.NoDocumentoCliente.Name = "NoDocumentoCliente";
+            this.NoDocumentoCliente.Size = new System.Drawing.Size(185, 20);
+            this.NoDocumentoCliente.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "NO. DOCUMENTO:";
+            // 
+            // TelefonoCliente
+            // 
+            this.TelefonoCliente.Location = new System.Drawing.Point(530, 138);
+            this.TelefonoCliente.Name = "TelefonoCliente";
+            this.TelefonoCliente.Size = new System.Drawing.Size(185, 20);
+            this.TelefonoCliente.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "TELÉFONO:";
+            // 
+            // ApellidoCliente
+            // 
+            this.ApellidoCliente.Location = new System.Drawing.Point(140, 193);
+            this.ApellidoCliente.Name = "ApellidoCliente";
+            this.ApellidoCliente.Size = new System.Drawing.Size(185, 20);
+            this.ApellidoCliente.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "DIRECCION:";
+            // 
+            // NombreCliente
+            // 
+            this.NombreCliente.Location = new System.Drawing.Point(140, 136);
+            this.NombreCliente.Name = "NombreCliente";
+            this.NombreCliente.Size = new System.Drawing.Size(185, 20);
+            this.NombreCliente.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "APELLIDOS:";
+            // 
+            // DireccionCliente
+            // 
+            this.DireccionCliente.Location = new System.Drawing.Point(140, 247);
+            this.DireccionCliente.Name = "DireccionCliente";
+            this.DireccionCliente.Size = new System.Drawing.Size(185, 20);
+            this.DireccionCliente.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOMBRES:";
+            // 
+            // btnRegsitrarCliente
+            // 
+            this.btnRegsitrarCliente.Location = new System.Drawing.Point(336, 341);
+            this.btnRegsitrarCliente.Name = "btnRegsitrarCliente";
+            this.btnRegsitrarCliente.Size = new System.Drawing.Size(105, 37);
+            this.btnRegsitrarCliente.TabIndex = 10;
+            this.btnRegsitrarCliente.Text = "REGISTRAR";
+            this.btnRegsitrarCliente.UseVisualStyleBackColor = true;
+            this.btnRegsitrarCliente.Click += new System.EventHandler(this.btnRegsitrarCliente_Click);
+            // 
+            // CBoxTipoDocuCliente
+            // 
+            this.CBoxTipoDocuCliente.FormattingEnabled = true;
+            this.CBoxTipoDocuCliente.Items.AddRange(new object[] {
+            "Cedula De Ciudadania",
+            "Cedula Extranjera ",
+            "PEP ",
+            "Pasaporte"});
+            this.CBoxTipoDocuCliente.Location = new System.Drawing.Point(530, 80);
+            this.CBoxTipoDocuCliente.Name = "CBoxTipoDocuCliente";
+            this.CBoxTipoDocuCliente.Size = new System.Drawing.Size(185, 21);
+            this.CBoxTipoDocuCliente.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "TIPO DE DOCUMENTO:";
+            // 
+            // CorreoCliente
+            // 
+            this.CorreoCliente.Location = new System.Drawing.Point(530, 193);
+            this.CorreoCliente.Name = "CorreoCliente";
+            this.CorreoCliente.Size = new System.Drawing.Size(185, 20);
+            this.CorreoCliente.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(468, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "CORREO:";
+            // 
+            // FromRegistrarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 444);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FromRegistrarCliente";
+            this.Text = "FromRegistrarCliente";
+            this.Load += new System.EventHandler(this.FromRegistrarCliente_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox NoDocumentoCliente;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TelefonoCliente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ApellidoCliente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NombreCliente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DireccionCliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegsitrarCliente;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox CBoxTipoDocuCliente;
+        private System.Windows.Forms.TextBox CorreoCliente;
+        private System.Windows.Forms.Label label7;
+    }
+}
